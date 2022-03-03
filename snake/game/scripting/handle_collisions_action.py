55d7d9e9f7c13f661524de9abbe22fe9e3434e3b
rodrigoloyo <rmloyo@byui.edu>
2022-03-03 08:37:25 -0600

    collision handling

--- snake/game/scripting/handle_collisions_action.py
+++ snake/game/scripting/handle_collisions_action.py
@@ -67,10 +67,25 @@ class HandleCollisionsAction(Action):
         snake = cast.get_first_actor("snakes")
         head = snake.get_segments()[0]
         segments = snake.get_segments()[1:]
-        
-        for segment in segments:
-            if head.get_position().equals(segment.get_position()):
-                self._is_game_over = True
+        snake1 = cast.get_first_actor("snakes1")
+        head1 = snake1.get_segments()[0]
+        segments1 = snake1.get_segments()[1:]
+        for segment1 in segments1:
+            for segment in segments:
+                if head.get_position().equals(segment.get_position()):
+                    
+                    self._is_game_over = True
+                elif  head.get_position().equals(segment1.get_position()):    
+                     self._is_game_over = True
+            
+                if head1.get_position().equals(segment1.get_position()):
+                    
+                    self._is_game_over = True
+                elif  head1.get_position().equals(segment.get_position()):    
+                      self._is_game_over = True
+
+
+                     
         
     def _handle_game_over(self, cast):
         """Shows the 'game over' message and turns the snake and food white if the game is over.
@@ -83,7 +98,8 @@ class HandleCollisionsAction(Action):
             segments = snake.get_segments()
             food = cast.get_first_actor("foods")
             food_1 = cast.get_first_actor("foods")
-            segments_1 = snake.get_segments()
+            snake1 = cast.get_first_actor("snakes1")
+            segments_1 = snake1.get_segments()
 
             x = int(constants.MAX_X / 2)
             y = int(constants.MAX_Y / 2)
