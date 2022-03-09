6497f3aeac4892bce84fcacf6fd0437be1176ab0
rodrigoloyo <rmloyo@byui.edu>
2022-03-08 20:38:50 -0600

    completed

--- snake/game/scripting/move_actors_action.py
+++ snake/game/scripting/move_actors_action.py
@@ -4,14 +4,7 @@ from game.scripting.action import Action
 
 class MoveActorsAction(Action):
         
-    # TODO: Implement MoveActorsAction class here! 
-
-    # Override the execute(cast, script) method as follows:
-    # 1) get all the actors from the cast
-    # def MoveActorsAction():
-    #     all_actor = get_actors()
-    # 2) loop through the actors
-    # 3) call the move_next() method on each actor
+    """Execute Methods modified"""    
     def execute(self, cast, script):
         """Executes the draw actors action.
 
